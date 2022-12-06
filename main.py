0fcfa6d9b0b0424373210d29c16adc5810e1efdb
Panzer-6 <omegax13579@gmail.com>
2022-12-06 10:29:38 +0700

    from Panzer-6: Re-organized into modules and implement UART reading

--- main.py
+++ main.py
@@ -1,55 +1,32 @@
 import  sys
 import random
 import time
-from Adafruit_IO import MQTTClient
+from adafruit import *
 from AI_Python import *
-
-
-AIO_FEED_ID = ["Temp_button","Pump_button"]
-AIO_USERNAME = "HCMUT_IOT"
-AIO_KEY = "aio_IhZL44jpgMiZiuIxiV3Fe4ShZzVB"
-
-def connected(client):
-    print("Ket noi thanh cong ...")
-    for id in AIO_FEED_ID:
-        client.subscribe(id)
-
-def subscribe(client , userdata , mid , granted_qos):
-    print("Subscribe thanh cong ...")
-
-def disconnected(client):
-    print("Ngat ket noi ...")
-    sys.exit(1)
-
-def message(client , feed_id , payload):
-    print("Data from " + feed_id + ":" + payload)
-
-
-client = MQTTClient(AIO_USERNAME, AIO_KEY)
-client.on_connect = connected
-client.on_disconnect = disconnected
-client.on_message = message
-client.on_subscribe = subscribe
-client.connect()
-client.loop_background()
-
+from timer import *
+from uart import *
 
 counter_ai = 60
 counter_sensor = 60
+setTimer(0, 1)
+setTimer(1, 1)
 while True:
-    counter_sensor = counter_sensor -1
-    counter_ai = counter_ai - 1
-    if counter_ai <= 0:
-        counter_ai = 60
+    # counter_sensor = counter_sensor -1
+    # counter_ai = counter_ai - 1
+    timerRun()
+    if timerTimeout(0):
+        setTimer(0, 2)
         image_capture()
         ai_result = image_detector()
         client.publish("AI", ai_result)
 
-    if counter_sensor <= 0:
-        counter_sensor = 60
+    if timerTimeout(1):
+        setTimer(1, 3)
         temp = random.randint(0,100)
         humid = random.randint(0,100)
         print(temp," ",humid)
         client.publish("Temp_sensor", temp)
         client.publish("Humid_sensor", humid)
+
+    readSerial(client)
     time.sleep(1)
